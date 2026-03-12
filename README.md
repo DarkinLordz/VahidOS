b4454cc84b22a7b210efb2f1eb36c4a806b9005e
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-12 18:23:33 +0400

    Added print function

--- README.md
+++ README.md
@@ -7,9 +7,8 @@ VahidOS is an experimental kernel.
 Requirements:
 - `i686-elf-gcc` cross compiler
 - `grub-mkrescue`
-- `qemu-system-i386` (for running)
 
 Commands:
 - `make` builds `kernel.bin`
 - `make iso` creates `VahidOS.iso`
-- `make run` boots the ISO in QEMU
+- `make clean` cleans binaries
--- src/kernel.c
+++ src/kernel.c
@@ -2,15 +2,15 @@
 #include <stdint.h>
 
 static volatile uint16_t *const VGA_BUFFER = (uint16_t *)0xB8000;
-static const uint8_t VGA_COLOR = 0x0F; /* White on black */
+static const uint8_t VGA_COLOR = 0x0F;
 
-void kernel_main(void) {
-    const char *msg = "Hello from VahidOS kernel!";
-
+void print(const char *msg) {
     for (size_t i = 0; msg[i] != '\0'; ++i) {
         VGA_BUFFER[i] = (uint16_t)msg[i] | ((uint16_t)VGA_COLOR << 8);
     }
+}
 
+void kernel_main(void) {
     for (;;) {
         __asm__ volatile ("hlt");
     }
