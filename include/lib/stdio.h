c3311b5bbffb5a441f058ad2fd201c413a38b06b
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-07-04 18:20:45 +0400

    Clean up tetost's code because shit sucks

--- include/lib/stdio.h
+++ include/lib/stdio.h
@@ -1,9 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (C) 2026 Vahid Khalafov */
+
+#ifndef VAHIX_STDIO_H
+#define VAHIX_STDIO_H
+
 #include <stdint.h>
 #include <stdarg.h>
 #include <drivers/vga.h>
-
 
 void print_string(const char *string);
 void print_hex_byte(uint8_t value);
 void print_hex(uint32_t value);
 int printf(const char * format, ...);
+
+#endif /* VAHIX_STDIO_H */
